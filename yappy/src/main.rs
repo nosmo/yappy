ab990fce2666cfc175f0a19e8a8e04eb03887d1e
Hugh Nowlan <nosmo@nosmo.me>
2021-08-28 15:11:06 +0100

    Add log level, move TCP check to function.
    Log depending on verbosity level. Also do some cleanup in general of
    learning stuff. Move tcp port check to a function.

--- yappy/src/main.rs
+++ yappy/src/main.rs
@@ -1,34 +1,71 @@
-use ferris_says::say; // from the previous step
 use std::io::{stdout, BufWriter};
+use std::env;
+use std::net::TcpStream;
+
+extern crate log;
+use log::{LevelFilter, debug, trace};
+
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+extern crate simple_logger;
+use simple_logger::SimpleLogger;
 
 // this is a comment
 /* this is a comment */
 /// this one is a docstring that is general
 
-use std::net::TcpStream;
-
-// Chemical spill Mutation world domination Methamphetamine Malware
-// industrial espionage Quarnantine Rubin Vaccine Al Qaida China Recce
-// FMS Iraq Iran
-
-// Power lines AGT. AMME Albright Jet State of emergency remailers
-// Halibut Emergency management Ron Brown NORAD Vince Foster Consul
-// Cartel de Golfo Weapons grade Vickie Weaver
-
-// AIEWS ISA FCIC FX LLC Gorelick Sleet Cartel National Guard CBNRC CBP
-// Drug war Fort Hancock BOP Adriatic
-
 const MAXPORT: i32 = 65535;
+
+fn tcp_check_port(host: &str, port: i32) -> bool {
+    let hostport = format!("{}:{}", host, port);
+
+    if let Ok(_stream) = TcpStream::connect(hostport) {
+        debug!("TCP port {} open", port);
+        return true;
+    } else {
+        trace!("TCP port {} closed", port);
+        return false;
+    }
+}
 
 fn main() {
 
     //! this is a docstring that is line specific
-    let pi = 3.141592;
-    println!("Hello {1} is {2:.0$}", 5, "x", 0.01);
-    println!("pi is exactly {1:.0$}", 4, pi);
+    let matches = App::new("yappy")
+        .version("1.0")
+        .author("hugh <nosmo@nosmo.me>")
+        .about("Yet Another Portscanner Programming Yoject")
+        .arg(Arg::with_name("host")
+             .help("The host to scan")
+             .required(true)
+             .index(1))
+        .arg(Arg::with_name("v")
+             .short("v")
+             .multiple(true)
+             .help("Print verbose output"))
+        .get_matches();
 
-    //let ip = "127.0.0.1";
-    let ip = "192.168.0.34";
+    match matches.occurrences_of("v") {
+        0 => simple_logger::SimpleLogger::new()
+            .with_level(log::LevelFilter::Error)
+            .init()
+            .unwrap(),
+        1 => simple_logger::SimpleLogger::new()
+            .with_level(log::LevelFilter::Debug)
+            .init()
+            .unwrap(),
+        2 => simple_logger::SimpleLogger::new()
+            .with_level(log::LevelFilter::Trace)
+            .init()
+            .unwrap(),
+        3 | _ => println!("Don't be crazy"),
+    }
+
+    //let args: Vec<String> = env::args().collect();
+    //println!("{:?}", args);
+    let ip = matches.value_of("host").unwrap();
+    //let filename = &args[2];
+
     let port = 22;
 
     let port_start = 1;
@@ -37,37 +74,15 @@ fn main() {
 
     let protocol = "tcp";
 
-    #[derive(Debug)]
-    struct DebugPrintable(i32);
-
-    println!("Now {:?} will print!", DebugPrintable(123));
-    println!("Now {:#?} will pretty print!", DebugPrintable(123));
-
-    println!("ACAB {} {} {} {0} {2} {1}", 1312i64,
-             "butts", 123.1234);
-    // the 1312i64 means that 1312 is 64 bit
-    println!("{butt:>width$}", butt=1, width=62);
-    println!("{butt:>0width$}", butt=2, width=62);
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
-
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
-
     let mut port_succ = 0;
     let mut port_fail = 0;
 
     let port_range = port_start..port_end;
 
     for port_range_port in port_range {
-        let hostport = format!("{}:{}", ip, port_range_port);
-
-        if let Ok(_stream) = TcpStream::connect(hostport) {
-            println!("TCP port {} open", port_range_port);
+        if tcp_check_port(ip, port_range_port) {
             port_succ += 1;
         } else {
-            //println!("Couldn't connect on port {}.", port_range);
             port_fail += 1;
         }
     }
